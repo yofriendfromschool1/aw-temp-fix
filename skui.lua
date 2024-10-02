12dbc4488e3f46e649d34189624207c5c12c68cc
yofriendfromschool1 <153390719+yofriendfromschool1@users.noreply.github.com>
2024-10-02 14:14:07 +0000

    Update skui.lua

--- skui.lua
+++ skui.lua
@@ -225,7 +225,13 @@ TabList.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TabList.BorderSizePixel = 0
 TabList.Position = UDim2.new(0, 0, 0.13333334, 0)
 TabList.Size = UDim2.new(0, 120, 0, 125)
-TabList.CanvasSize = UDim2.new(0, 0, 1.5, 0)
+local function updateCanvasSize()
+    local contentSize = TabList.UIGridLayout.AbsoluteContentSize
+    TabList.CanvasSize = UDim2.new(0, contentSize.X, 0, contentSize.Y)
+end
+
+TabList.UIGridLayout:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(updateCanvasSize)
+updateCanvasSize()
 TabList.ScrollBarThickness = 0
 
 UIGridLayout.Parent = TabList
